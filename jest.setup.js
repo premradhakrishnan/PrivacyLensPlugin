50a41207bb99c9d1f4159d5ca03cc3ccdbb7f993
Prem Radhakrishnan [CEnet] <58406844+premradhakrishnan@users.noreply.github.com>
2025-02-12 12:35:50 +1100

    Saving work

---
+++ jest.setup.js
@@ -0,0 +1,17 @@
+import '@testing-library/jest-dom';
+
+// Mock Chrome API
+global.chrome = {
+  runtime: {
+    sendMessage: jest.fn(),
+    onMessage: {
+      addListener: jest.fn()
+    }
+  },
+  storage: {
+    local: {
+      get: jest.fn(),
+      set: jest.fn()
+    }
+  }
+};
